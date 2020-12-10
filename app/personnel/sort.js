3e93147d33fb48a44238d0d647b1deb0a9c115ca
Alex <razielakaalien@outlook.com>
2020-12-10 06:41:30 +0000

    Minor fixes

--- app/personnel/sort.js
+++ app/personnel/sort.js
@@ -44,18 +44,18 @@ function sortBy(sortToggle) {
         }
         
         readPersonnelHtml += `
-            <div class="row justify-content-center text-center text-md-left py-2 md-mb-1 info-panel">
-                <div class="col-6 col-md-2 align-self-center rounded-top data-panel"><i class="fas fa-user d-inline d-md-none"></i> ${fullName}</div>
+            <div class="row justify-content-center text-center text-md-left py-1 md-mb-1 info-panel">
+                <div class="col-11 col-sm-8 col-md-2 align-self-center rounded-top data-panel"><i class="fas fa-user d-inline d-md-none"></i> ${fullName}</div>
                 <div class="w-100 d-md-none d-block"></div>
-                <div class="col-6 col-md-2 align-self-center data-panel"><i class="fas fa-address-card d-inline d-md-none"></i> ${value.jobTitle}</div>
+                <div class="col-11 col-sm-8 ol-md-2 align-self-center data-panel"><i class="fas fa-address-card d-inline d-md-none"></i> ${value.jobTitle}</div>
                 <div class="w-100 d-md-none d-block"></div>
-                <div class="col-6 col-md-2 align-self-center text-truncate data-panel"><i class="fas fa-envelope d-inline d-md-none"></i> ${value.email}</div>
+                <div class="col-11 col-sm-8 col-md-2 align-self-center text-truncate data-panel"><i class="fas fa-envelope d-inline d-md-none"></i> ${value.email}</div>
                 <div class="w-100 d-md-none d-block"></div>
-                <div class="col-6 col-md-2 align-self-center data-panel"><i class="fas fa-network-wired d-inline d-md-none"></i> ${value.department}</div>
+                <div class="col-11 col-sm-8 col-md-2 align-self-center data-panel"><i class="fas fa-network-wired d-inline d-md-none"></i> ${value.department}</div>
                 <div class="w-100 d-md-none d-block"></div>
-                <div class="col-6 col-md-2 align-self-center data-panel"><i class="fas fa-map-marker-alt d-inline d-md-none"></i> ${value.location}</div>
+                <div class="col-11 col-sm-8 col-md-2 align-self-center data-panel"><i class="fas fa-map-marker-alt d-inline d-md-none"></i> ${value.location}</div>
                 <div class="w-100 d-md-none d-block"></div>
-                <div class="col-6 col-md-2 align-self-center text-nowrap rounded-bottom py-1 data-panel">
+                <div class="col-11 col-sm-8 col-md-2 align-self-center text-nowrap rounded-bottom py-2 data-panel">
                     <button class="btn btn-outline-primary btn-sm read-one-personnel-btn" data-id="${value.id}" data-depId="${value.departmentId}"><i class="far fa-eye"></i></button>
                     <button class="btn btn-outline-info btn-sm update-personnel-btn" data-id="${value.id}" data-depId="${value.departmentId}"><i class="far fa-edit"></i></button>
                     <button class="btn btn-outline-danger btn-sm delete-personnel-btn" data-id="${value.id}" data-depId="${value.departmentId}" data-toggle="modal" data-target="#deleteModalConfirmation"><i class="far fa-trash-alt"></i></button>
